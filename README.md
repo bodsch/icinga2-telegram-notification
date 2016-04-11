251b6d0ceb686f8ab2b0e36e510f774ba794c7d9
Bodo Schulz <support@xanhaem.de>
2016-04-11 11:33:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Icinga2 Notifications with Telegram
+
+2 simple Scripts to use customized TelegramBot for Notifications
---
+++ telegram-service-notification.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TELEGRAM_API_KEY=
+
+if [ -z ${TELEGRAM_API_KEY} ]
+then
+  echo "kein API KEY vorhanden"
+  exit 2
+fi
+
+ARCHIV_DIR="/var/log/icinga2/notifications"
+
+[ -d ${ARCHIV_DIR} ] || mkdir -p ${ARCHIV_DIR}
+
+# -----------------------------------------------------------------------------
+
+template=$(cat <<TEMPLATE
+ > $NOTIFICATIONTYPE <
+
+Service: $SERVICEDESC
+Host   : $HOSTALIAS
+Address: $HOSTADDRESS
+State  : $SERVICESTATE
+
+Date/Time: $LONGDATETIME
+
+Additional Info: $SERVICEOUTPUT
+
+Comment: [$NOTIFICATIONAUTHORNAME] $NOTIFICATIONCOMMENT
+TEMPLATE
+)
+
+# -----------------------------------------------------------------------------
+
+echo "${template}" > /var/tmp/notification.body
+
+/usr/bin/curl -X  POST --data chat_id=${USERTELEGRAM} --data text="$(cat /var/tmp/notification.body)" https://api.telegram.org/bot${TELEGRAM_API_KEY}/sendMessage
+
+mv /var/tmp/notification.body ${ARCHIV_DIR}/$(date +"%Y%m%d_%H%M%S").body
+
+exit $?
+
